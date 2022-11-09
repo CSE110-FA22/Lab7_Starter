7ababa96d3631806539b439d83781eeae191103a
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-11-09 08:49:31 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -47,7 +47,7 @@ function initializeServiceWorker() {
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register '/sw.js' as a service worker (The MDN article
+  // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
