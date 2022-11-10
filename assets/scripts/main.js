5c2fe32bfb96a6c5a84df4bb275435589cb333cd
Atomgroup530 <xul007@ucsd.edu>
2022-11-10 13:36:21 -0800

    Correct A9 location, adjust commen location

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -89,51 +89,46 @@ async function getRecipes() {
 
   return new Promise(async (resolve, reject) =>
   {
+    // A4-A11 will all be *inside* the callback function we passed to the Promise
+    // we're returning
+    /**************************/
+    // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
+    //            declared above
     for (const url of RECIPE_URLS)
     {
+      // A5. TODO - Since we are going to be dealing with asynchronous code, create
+      //            a try / catch block. A6-A9 will be in the try portion, A10-A11
+      //            will be in the catch portion.
       try
       {
-        // A4-A11 will all be *inside* the callback function we passed to the Promise
-        // we're returning
-        /**************************/
-        // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
-        //            declared above
-        // A5. TODO - Since we are going to be dealing with asynchronous code, create
-        //            a try / catch block. A6-A9 will be in the try portion, A10-A11
-        //            will be in the catch portion.
         // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
         //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
         //            you must either use "await fetch(...)" or "fetch.then(...)". This
         //            function is using the async keyword so we recommend "await"
+        recipe = await fetch(url);
         // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
         //            NOTE: .json() is ALSO asynchronous, so you will need to use
         //            "await" again
+        recipe = await recipe.json();
         // A8. TODO - Add the new recipe to the recipes array
+        recipes.push(recipe);
         // A9. TODO - Check to see if you have finished retrieving all of the recipes,
         //            if you have, then save the recipes to storage using the function
         //            we have provided. Then, pass the recipes array to the Promise's
         //            resolve() method.
-        recipe = await fetch(url);
-        recipe = await recipe.json();
-        recipes.push(recipe);
+        if (recipes.length === RECIPE_URLS.length)
+        {
+          saveRecipesToStorage(recipes);
+          resolve(recipes);
+        }
       }
       catch(error)
       {
         // A10. TODO - Log any errors from catch using console.error
-        // A11. TODO - Pass any errors to the Promise's reject() function
         console.error(error);
+        // A11. TODO - Pass any errors to the Promise's reject() function
         reject(error);
       }
-    }
-
-    if (recipes.length === RECIPE_URLS.length)
-    {
-      saveRecipesToStorage(recipes);
-      resolve(recipes);
-    }
-    else
-    {
-      reject("Not all recipes retrieved");
     }
   });
 }
