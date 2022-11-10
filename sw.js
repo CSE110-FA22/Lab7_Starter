cee90e588854e6a0115e417661488fd00e4abee0
Sia Sheth <snsheth@ucsd.edu>
2022-11-10 14:01:46 -0800

    async edit

--- sw.js
+++ sw.js
@@ -39,13 +39,12 @@ self.addEventListener('fetch', function (event) {
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
   /*******************************/
-  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-    return cache.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request).then((fetchedResponse) => {
-        cache.put(event.request, fetchedResponse.clone());
-        return fetchedResponse;
-      })
-    })
+  event.respondWith(caches.open(CACHE_NAME).then(async (cache) => {
+    const cachedResponse = await cache.match(event.request);
+    return cachedResponse || fetch(event.request).then((fetchedResponse) => {
+      cache.put(event.request, fetchedResponse.clone());
+      return fetchedResponse;
+    });
   }))
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
