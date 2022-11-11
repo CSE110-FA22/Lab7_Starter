ec2057e7f40abcdf2963bbb7c8f856a223a787fe
JerryW <ziw081@ucsd.edu>
2022-11-10 23:20:43 -0800

    attempt to fix stars not appearing

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -133,7 +133,7 @@ class RecipeCard extends HTMLElement {
       <p class="organization">${data.organization}</p>
       <div class="rating">
         <span>${data.rating}</span>
-        <img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+        <img src="./assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
         <span>(${data.numRatings})</span>
       </div>
       <time>${data.lengthTime}</time>
