1b95c174ae909f79bf770d2ec9da745a93be0623
josh-tan-20-09-13 <joshua555op@gmail.com>
2023-05-16 18:41:28 -0700

    Finished explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,31 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if ("serviceWorker" in navigator) {
+  
+    // B2. TODO - Listen for the 'load' event on the window object.
+    self.addEventListener("load", async (event) => {
+
+    
+    // Steps B3-B6 will be *inside* the event listener's function created in B2
+    // B3. TODO - Register './sw.js' as a service worker (The MDN article
+    //            "Using Service Workers" will help you here)
+      try {
+        await navigator.serviceWorker.register("./sw.js");
+    
+    // B4. TODO - Once the service worker has been successfully registered, console
+    //            log that it was successful.
+        console.log("Successfulled registered service worker");
+    
+      } catch (error) {
+    // B5. TODO - In the event that the service worker registration fails, console
+    //            log that it has failed.
+        console.log("Failed to register service worker");
+      }
+    });
+    
+    // STEPS B6 ONWARDS WILL BE IN /sw.js
+  }
 }
 
 /**
--- sw.js
+++ sw.js
@@ -9,7 +9,14 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      return cache.addAll([
+        'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
+        'https://introweb.tech/assets/json/2_roasting-turkey-breast-with-stuffing.json',
+        'https://introweb.tech/assets/json/3_moms-cornbread-stuffing.json',
+        'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',
+      ]);
     })
   );
 });
@@ -34,7 +41,18 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    // B8. TODO - If the request is in the cache, return with the cached version.
+    //            Otherwise fetch the resource, add it to the cache, and return
+    //            network response.
+    return cache.match(event.request).then((cachedResponse) => {
+      if (cachedResponse)
+        return cachedResponse;
+
+      return fetch(event.request).then((fetchedResponse) => {
+        cache.put(event.request, fetchedResponse.clone());
+        return fetchedResponse;
+      });
+    });
+  }));
 });
